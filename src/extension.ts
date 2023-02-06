91457814e74e9b64551d060732c457106dd624ce
Yan Zhang <yanzh@microsoft.com>
2023-02-06 16:55:32 +0800

    test publish with proposedApi

--- src/extension.ts
+++ src/extension.ts
@@ -18,6 +18,32 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage(`Platform: ${buffer.toString()}`);
 	}));
 
+	const provider: vscode.TreeDataProvider<string> = {
+		getTreeItem: function (element: string): vscode.TreeItem | Thenable<vscode.TreeItem> {
+			const item = new vscode.TreeItem2(element);
+			item.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
+			if (vscode.TreeItemCheckboxState !== undefined) {
+				item.checkboxState = vscode.TreeItemCheckboxState.Checked;
+				item.iconPath = vscode.ThemeIcon.File;
+			} else {
+				item.iconPath = new vscode.ThemeIcon("azure");
+			}
+			return item;
+		},
+		getChildren: function (element?: string | undefined): vscode.ProviderResult<string[]> {
+			if (!element) {
+				return ["Root"];
+			} else if (element === "Root") {
+				return ["R", "o", "o", "t"];
+			} else {
+				return [];
+			}
+		}
+	};
+	const treeView = vscode.window.createTreeView("myView", {
+		treeDataProvider: provider
+	});
+	context.subscriptions.push(treeView);
 }
 
 export function deactivate() {}
